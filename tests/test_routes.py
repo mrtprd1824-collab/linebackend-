dff6749bb6330ca849a2316ade38f309a3725124
mrtprd1824-collab <mr.tprd1824@gmail.com>
2025-10-15 19:56:02 +0700

    Remove tests, pytest config and CI workflow

--- tests/test_routes.py
+++
@@ -1,3 +0,0 @@
-def test_index_redirect(client):
-    rv = client.get("/")
-    assert rv.status_code in (200, 302)
--- tests/test_routes_index.py
+++
@@ -1,5 +0,0 @@
-def test_index_redirect(client):
-    rv = client.get("/")
-    assert rv.status_code in (200, 302)
-    if rv.status_code == 302:
-        assert "/login" in rv.headers.get("Location", "")
